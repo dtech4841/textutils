500c87fff42284ebf77c3416c63221416e40540f
dtech4841 <139841855+dtech4841@users.noreply.github.com>
2023-08-28 23:42:28 +0530

    Updates

---
+++ precache-manifest.29264ad21d63a6b601188b91fef14777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3f6f6f5837267fa6619",
+    "url": "/textutils/static/css/main.dde870d0.chunk.css"
+  },
+  {
+    "revision": "d3f6f6f5837267fa6619",
+    "url": "/textutils/static/js/main.d3f6f6f5.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/textutils/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "08e16b0eb3348270595a",
+    "url": "/textutils/static/js/2.08e16b0e.chunk.js"
+  },
+  {
+    "revision": "6d8377050a97b48f66d5",
+    "url": "/textutils/static/js/runtime~main.6d837705.js"
+  },
+  {
+    "revision": "053dbdeed93bb5db8ac442a3a4d0423a",
+    "url": "/textutils/index.html"
+  }
+];
